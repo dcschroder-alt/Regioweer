6d21bc4451198898501680d8cb6527368bf5b146
dcschroder-alt <dcschroder@gmail.com>
2026-09-23 17:21:01 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,8 @@
-const CACHE='regioweer-v93';
+const CACHE='regioweer-v94';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('regioweer-')&&k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
-  const u=new URL(e.request.url);
-  if(u.hostname==='raw.githubusercontent.com'||u.pathname.endsWith('/data/debilt-actueel.json')){
-    e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));
-    return;
-  }
   e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request)));
 });
