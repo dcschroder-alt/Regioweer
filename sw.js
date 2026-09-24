076980b0d0501501fae78f3579336cf0ff44c7b5
dcschroder-alt <dcschroder@gmail.com>
2026-09-24 23:14:04 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'regioweer-v1-0-finemesh-365';
+const CACHE='regioweer-v1-1-climate-outlook-v108';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('regioweer-')&&k!==CACHE).map(k=>caches.delete(k))))])));
